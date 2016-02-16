e564330d9db8804665a9a51b4a6d1b129ef26fdb
Alexander Khryukin <alexander@mezon.ru>
2016-02-16 03:28:14 +0300

    run mock without sudo

--- iso-builder/entrypoint.sh
+++ iso-builder/entrypoint.sh
@@ -15,12 +15,16 @@ source /etc/profile
 echo "prepare ABF ISO builder environment"
 echo "git clone docker-iso-worker code"
 cd
+if [ ! -d "${HOME}/docker-iso-worker" ]; then
 git clone https://github.com/OpenMandrivaSoftware/docker-iso-worker.git
+else
+rm -rf ${HOME}/docker-iso-worker
+git clone https://github.com/OpenMandrivaSoftware/docker-iso-worker.git
+fi
 pushd docker-iso-worker
 export PATH="${PATH}:/usr/local/rvm/bin"
 # skip $ARCH before we build hiredis gem
 unset ARCH
-which rvm
 gem install bundler
 bundle install
 REDIS_HOST=$REDIS_HOST REDIS_PORT=$REDIS_PORT REDIS_PASSWORD=$REDIS_PASSWORD QUEUE=$QUEUE COUNT=$COUNT BUILD_TOKEN=$BUILD_TOKEN rake resque:work
