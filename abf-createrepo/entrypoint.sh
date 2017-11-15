6a25c1aa22ed55336aa3959b883db3a4389df27a
Alexander Khryukin <alexander@mezon.ru>
2017-11-16 01:16:44 +0300

    adjust createrepo dockerfile to enable contrib too

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/x86_64/main/release/
+createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/x86_64/contrib/release/
 createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/aarch64/main/release/
+createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/aarch64/contrib/release/
