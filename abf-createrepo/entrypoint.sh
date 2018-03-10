ba951ffa4c8a3781d1108ea561d15e16753e3577
tpgxyz <tpgxyz@gmail.com>
2018-03-10 17:52:36 +0100

    update createrepo to use dnf and create repository metadata

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
-createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/x86_64/main/release/
-createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/x86_64/contrib/release/
-createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/aarch64/main/release/
-createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/aarch64/contrib/release/
+#!/bin/sh
+
+REPO=${$1:-"/share/platforms/cooker/repository"}
+
+for i in i686 x86_64 aarch64 armv7hl SRPMS; do
+    for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
+	for k in release updates testing; do
+	    printf '%s\n' "Regenerating repodata in ${REPO}/${i}/${j}/${k}"
+	    createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz --update "${REPO}"/repository/"${i}"/"${j}"/"${k}"
+	    rc=$?
+	    if [ "${rc}" != '0' ]; then
+		printf '%s\n' "Failed regenerating repodata in ${REPO}/${i}/${j}/${k}"
+	    else
+		printf '%s\n' "Fnished regenerating repodata in ${REPO}/${i}/${j}/${k}"
+	    fi
+	done
+    done
+done
