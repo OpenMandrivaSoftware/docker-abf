e0420123c64add153df44ab16ecb86971cad557e
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2018-03-10 18:18:24 +0100

    adjust for 3.0

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -1,18 +1,5 @@
-#!/bin/sh
-
-REPO=${REPOSITORY:-"/share/platforms/cooker/repository"}
-
-for i in i686 x86_64 aarch64 armv7hl SRPMS; do
-    for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
-	for k in release updates testing; do
-	    printf '%s\n' "Regenerating repodata in ${REPO}/${i}/${j}/${k}"
-	    createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz --update "${REPO}"/"${i}"/"${j}"/"${k}"
-	    rc=$?
-	    if [ "${rc}" != '0' ]; then
-		printf '%s\n' "Failed regenerating repodata in ${REPO}/${i}/${j}/${k}"
-	    else
-		printf '%s\n' "Fnished regenerating repodata in ${REPO}/${i}/${j}/${k}"
-	    fi
-	done
-    done
-done
+#!/bin/bash
+createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/x86_64/main/release/
+createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/x86_64/contrib/release/
+createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/aarch64/main/release/
+createrepo_c --no-database --update --workers=10 --simple-md-filenames --general-compress-type=xz /share/platforms/cooker/repository/aarch64/contrib/release/
