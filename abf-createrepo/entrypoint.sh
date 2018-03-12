86b10f3daaadf0afedb74527fc316a0866777dc3
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2018-03-12 15:30:22 +0100

    get rid of .repodata

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -8,6 +8,7 @@ for i in i686 x86_64 aarch64 armv7hl SRPMS; do
 	    printf '%s\n' "Starting regenerating repodata in ${REPO}/${i}/${j}/${k}"
 		if [ ! -e "${REPO}"/"${i}"/"${j}"/"${k}"/repodata ]; then
 			printf '%s\n' "Regenerating repodata from scratch in ${REPO}/${i}/${j}/${k}"
+			rm -rf "${REPO}"/"${i}"/"${j}"/"${k}"/.repodata
 			mkdir -p "${REPO}"/"${i}"/"${j}"/"${k}"/repodata
 			chown root:root ${REPO}/${i}/${j}/${k}/repodata
 			chmod 0755 ${REPO}/${i}/${j}/${k}/repodata
@@ -15,13 +16,17 @@ for i in i686 x86_64 aarch64 armv7hl SRPMS; do
 			rc=$?
 		else
 			printf '%s\n' "Regenerating and updating repodata from scratch in ${REPO}/${i}/${j}/${k}"
+			if [ -e "${REPO}"/"${i}"/"${j}"/"${k}"/.repodata ]; then
+				printf '%s\n' "Previous .repodata exists in ${REPO}/${i}/${j}/${k}. Removing it."
+				rm -rf "${REPO}"/"${i}"/"${j}"/"${k}"/.repodata
+			fi
 			createrepo_c --no-database --workers=10 --general-compress-type=xz --update "${REPO}"/"${i}"/"${j}"/"${k}"
 			rc=$?
 		fi
 	    if [ "${rc}" != '0' ]; then
 		printf '%s\n' "Failed regenerating repodata in ${REPO}/${i}/${j}/${k}"
 	    else
-		printf '%s\n' "Fnished regenerating repodata in ${REPO}/${i}/${j}/${k}"
+		printf '%s\n' "Finished regenerating repodata in ${REPO}/${i}/${j}/${k}"
 	    fi
 	    chown root:root ${REPO}/${i}/${j}/${k}/*
 	    chmod 0666 ${REPO}/${i}/${j}/${k}/*
