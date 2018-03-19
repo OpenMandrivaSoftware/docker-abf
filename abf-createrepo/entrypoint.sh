80831d2f9c001de24c2e418571dae1fe33673eea
tpgxyz <tpgxyz@gmail.com>
2018-03-19 22:00:04 +0100

    allow to run createrepo_c on given path

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -1,43 +1,63 @@
 #!/bin/sh
 
-REPO=${REPOSITORY:-"/share/platforms/cooker/repository"}
+# Modified by tpgxyz@gmail.com
+# Call this script with path where createrepo_c will run
+# and call with regenerate paramater to build repodata
+# from scratch.
 
-[ ! -d "${REPO}" ] && printf '%s\n' "Directory ${REPO} does not exist. Exiting." && exit 1
+run_createrepo() {
+    REPOSITORY="$1"
+    REGENERATE="$2"
 
-for i in i686 x86_64 aarch64 armv7hl SRPMS; do
-    for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
-	for k in release updates testing; do
-	    printf '%s\n' "Starting regenerating repodata in ${REPO}/${i}/${j}/${k}"
-		if [ ! -e "${REPO}"/"${i}"/"${j}"/"${k}"/repodata ]; then
-			printf '%s\n' "Regenerating repodata from scratch in ${REPO}/${i}/${j}/${k}"
-			rm -rf "${REPO}"/"${i}"/"${j}"/"${k}"/.repodata
-			mkdir -p "${REPO}"/"${i}"/"${j}"/"${k}"/repodata
-			chown root:root "${REPO}"/"${i}"/"${j}"/"${k}"/repodata
-			chmod 0755 "${REPO}"/"${i}"/"${j}"/"${k}"/repodata
-			createrepo_c --no-database --workers=10 --general-compress-type=xz --ignore-lock "${REPO}"/"${i}"/"${j}"/"${k}"
-			rc=$?
-		else
-			printf '%s\n' "Regenerating and updating repodata in ${REPO}/${i}/${j}/${k}"
-			if [ -e "${REPO}"/"${i}"/"${j}"/"${k}"/.repodata ]; then
-				printf '%s\n' "Previous .repodata exists in ${REPO}/${i}/${j}/${k}. Removing it."
-				rm -rf "${REPO}"/"${i}"/"${j}"/"${k}"/.repodata
-			fi
-			createrepo_c --no-database --workers=10 --general-compress-type=xz --update "${REPO}"/"${i}"/"${j}"/"${k}"
-			rc=$?
-		fi
+    [ ! -d "${REPORITORY}" ] && printf '%s\n' "Directory ${REPORITORY} does not exist. Exiting." && exit 1
+    printf '%s\n' "Starting regenerating repodata in ${REPOSITORY}"
 
-	    if [ "${rc}" != '0' ]; then
-		printf '%s\n' "Failed regenerating repodata in ${REPO}/${i}/${j}/${k}"
-	    else
-		printf '%s\n' "Finished regenerating repodata in ${REPO}/${i}/${j}/${k}"
-	    fi
+    if [ ! -e "${REPOSITORY}"/repodata ] || [ "$2" = 'regenerate' ]; then
+	printf '%s\n' "Regenerating repodata from scratch in ${REPOSITORY}"
+	rm -rf "${REPOSITORY}"/.repodata
+	mkdir -p "${REPOSITORY}"/repodata
+	chown root:root "${REPOSITORY}"/repodata
+	chmod 0755 "${REPOSITORY}"/repodata
+	createrepo_c --no-database --workers=10 --general-compress-type=xz --ignore-lock "${REPOSITORY}"
+	rc=$?
+    else
+	printf '%s\n' "Regenerating and updating repodata in ${REPOSITORY}"
+	if [ -e "${REPOSITORY}"/.repodata ]; then
+	    printf '%s\n' "Previous .repodata exists in ${REPOSITORY}. Removing it."
+	    rm -rf "${REPOSITORY}"/.repodata
+	fi
+	    createrepo_c --no-database --workers=10 --general-compress-type=xz --update "${REPOSITORY}"
+	    rc=$?
+    fi
 
-	    if [ -e "${REPO}"/"${i}"/"${j}"/"${k}"/repodata ]; then
-		[ $(stat -c "%U" "${REPO}"/"${i}"/"${j}"/"${k}"/repodata ) != 'root' ] && chown root:root "${REPO}"/"${i}"/"${j}"/"${k}"/repodata
-		[ $(stat -c "%a" "${REPO}"/"${i}"/"${j}"/"${k}"/repodata ) != '755' ] && chmod 0755 "${REPO}"/"${i}"/"${j}"/"${k}"/repodata
-	    fi
+    if [ "${rc}" != '0' ]; then
+	printf '%s\n' "Failed regenerating repodata in ${REPOSITORY}"
+	else
+	    printf '%s\n' "Finished regenerating repodata in ${REPOSITORY}"
+    fi
+
+    if [ -e "${REPOSITORY}"/repodata ]; then
+	[ $(stat -c "%U" "${REPOSITORY}"/repodata ) != 'root' ] && chown root:root "${REPOSITORY}"/repodata
+	[ $(stat -c "%a" "${REPOSITORY}"/repodata ) != '755' ] && chmod 0755 "${REPOSITORY}"/repodata
+    fi
+
+}
+
+if [ -n "$1" ];
+    run_createrepo "$1" "$2"
+else
+
+    REPOSITORY="/share/platforms/cooker/repository"
+    [ ! -d "${REPORITORY}" ] && printf '%s\n' "Directory ${REPORITORY} does not exist. Exiting." && exit 1
+
+    for i in i686 x86_64 aarch64 armv7hl SRPMS; do
+	for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
+	    for k in release updates testing; do
+		run_createrepo "${REPORITORY}/${i}/${j}/${k}"
+	    done
 	done
     done
-done
+
+fi
 
 exit 0
