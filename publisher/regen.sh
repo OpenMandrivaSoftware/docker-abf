c3cf9eb8a2ad6ed038561b3add774a3e7413ec40
Alexander Khryukin <alexander@mezon.ru>
2018-05-26 13:27:22 +0300

    update regen script

--- publisher/regen.sh
+++ publisher/regen.sh
@@ -2,7 +2,7 @@
 if [ -z "$1" ]
    then
    /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/i686/main/release/ regenerate
-   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/armv7hl/main/release/ regenerate
+   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/armv7hnl/main/release/ regenerate
    /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/x86_64/main/release/ regenerate
    /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/aarch64/main/release/ regenerate
 else
