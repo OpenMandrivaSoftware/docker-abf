4851a382564c7bae786190a9a0f0642b98716b07
Bernhard Rosenkränzer <bero@lindev.ch>
2018-05-29 01:24:53 +0200

    Update regen.sh to allow rebuilding multiple arches
    Also allow parallel rebuilding

--- publisher/regen.sh
+++ publisher/regen.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-if [ -z "$1" ]
-   then
-   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/i686/main/release/ regenerate
-   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/armv7hnl/main/release/ regenerate
-   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/x86_64/main/release/ regenerate
-   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/aarch64/main/release/ regenerate
-else
-   /usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/$1/main/release/ regenerate
+#!/bin/sh
+PLATFORMS="$@"
+if [ -z "$PLATFORMS" ]; then
+	PLATFORMS="x86_64 aarch64 i686 armv7hnl"
 fi
+echo "Rebuilding metadata for $PLATFORMS"
+for i in $PLATFORMS; do
+	if ! [ -d /share/platforms/cooker/repository/$i/main/release ]; then
+		echo "$i doesn't seem to be a valid platform" >&2
+		continue
+	fi
+	/usr/bin/docker run --rm -v /home/abf-downloads:/share/platforms openmandriva/createrepo /share/platforms/cooker/repository/$i/main/release/ regenerate &
+done
+wait
