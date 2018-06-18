44cf5c90829cbeb92a70a82bdb675f9d2ab7b49c
Bernhard Rosenkränzer <bero@lindev.ch>
2018-06-18 20:39:22 +0200

    armv7hl -> armv7hnl

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -51,7 +51,7 @@ else
     REPOSITORY="/share/platforms/cooker/repository"
     [ ! -d "${REPOSITORY}" ] && printf '%s\n' "Directory ${REPOSITORY} does not exist. Exiting." && exit 1
 
-    for i in i686 x86_64 aarch64 armv7hl SRPMS; do
+    for i in i686 x86_64 aarch64 armv7hnl SRPMS; do
 	for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
 	    for k in release updates testing; do
 		run_createrepo "${REPOSITORY}/${i}/${j}/${k}"
