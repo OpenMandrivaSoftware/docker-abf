bd9ada5a84911d2b3546659713ba49516acf8fad
Bernhard Rosenkränzer <bero@lindev.ch>
2018-06-18 20:44:15 +0200

    Try full regeneration if metadata update fails

--- abf-createrepo/entrypoint.sh
+++ abf-createrepo/entrypoint.sh
@@ -14,32 +14,37 @@ run_createrepo() {
     printf '%s\n' "Starting regenerating repodata in ${REPOSITORY}"
 
     if [ ! -e "${REPOSITORY}"/repodata ] || [ "$2" = 'regenerate' ]; then
-	printf '%s\n' "Regenerating repodata from scratch in ${REPOSITORY}"
-	rm -rf "${REPOSITORY}"/.repodata
-	mkdir -p "${REPOSITORY}"/repodata
-	chown root:root "${REPOSITORY}"/repodata
-	chmod 0755 "${REPOSITORY}"/repodata
-	createrepo_c --no-database --workers=10 --general-compress-type=xz --ignore-lock "${REPOSITORY}"
-	rc=$?
+        printf '%s\n' "Regenerating repodata from scratch in ${REPOSITORY}"
+        rm -rf "${REPOSITORY}"/.repodata
+        mkdir -p "${REPOSITORY}"/repodata
+        chown root:root "${REPOSITORY}"/repodata
+        chmod 0755 "${REPOSITORY}"/repodata
+        createrepo_c --no-database --workers=10 --general-compress-type=xz --ignore-lock "${REPOSITORY}"
+        rc=$?
     else
-	printf '%s\n' "Regenerating and updating repodata in ${REPOSITORY}"
-	if [ -e "${REPOSITORY}"/.repodata ]; then
-	    printf '%s\n' "Previous .repodata exists in ${REPOSITORY}. Removing it."
-	    rm -rf "${REPOSITORY}"/.repodata
-	fi
+        printf '%s\n' "Regenerating and updating repodata in ${REPOSITORY}"
+        if [ -e "${REPOSITORY}"/.repodata ]; then
+            printf '%s\n' "Previous .repodata exists in ${REPOSITORY}. Removing it."
+            rm -rf "${REPOSITORY}"/.repodata
+        fi
         createrepo_c --no-database --workers=10 --general-compress-type=xz --update "${REPOSITORY}"
         rc=$?
+        if [ "${rc}" != '0' ]; then
+            printf '%s\n' "Failed updating repodata in ${REPOSITORY}, trying regeneration from scratch"
+            run_createrepo "${REPOSITORY}" "regenerate"
+            return
+        fi
     fi
 
     if [ "${rc}" != '0' ]; then
-	printf '%s\n' "Failed regenerating repodata in ${REPOSITORY}"
+        printf '%s\n' "Failed regenerating repodata in ${REPOSITORY}"
     else
         printf '%s\n' "Finished regenerating repodata in ${REPOSITORY}"
     fi
 
     if [ -e "${REPOSITORY}"/repodata ]; then
-	[ $(stat -c "%U" "${REPOSITORY}"/repodata ) != 'root' ] && chown root:root "${REPOSITORY}"/repodata
-	[ $(stat -c "%a" "${REPOSITORY}"/repodata ) != '755' ] && chmod 0755 "${REPOSITORY}"/repodata
+        [ $(stat -c "%U" "${REPOSITORY}"/repodata ) != 'root' ] && chown root:root "${REPOSITORY}"/repodata
+        [ $(stat -c "%a" "${REPOSITORY}"/repodata ) != '755' ] && chmod 0755 "${REPOSITORY}"/repodata
     fi
 
 }
@@ -52,11 +57,11 @@ else
     [ ! -d "${REPOSITORY}" ] && printf '%s\n' "Directory ${REPOSITORY} does not exist. Exiting." && exit 1
 
     for i in i686 x86_64 aarch64 armv7hnl SRPMS; do
-	for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
-	    for k in release updates testing; do
-		run_createrepo "${REPOSITORY}/${i}/${j}/${k}"
-	    done
-	done
+        for j in main contrib non-free restricted debug_main debug_contrib debug_non-free debug_restricted; do
+            for k in release updates testing; do
+                run_createrepo "${REPOSITORY}/${i}/${j}/${k}"
+            done
+        done
     done
 
 fi
