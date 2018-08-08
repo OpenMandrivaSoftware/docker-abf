e78b25eb7befef4deb38d7dc3057a81b95573196
tpgxyz <tpgxyz@gmail.com>
2018-08-08 19:26:51 +0200

    generate amount of broken packages

--- abf-repoclosure/entrypoint.sh
+++ abf-repoclosure/entrypoint.sh
@@ -15,25 +15,31 @@ tr.fixed { background-color: green; }
 
 <h1>DNF repoclosure report</h1>
 <p>This report has been updated on $(date). <br />
-<table class="contents" style="width: 159px;">
+<table class="contents" style="width: 328px;">
 <tbody>
 <tr>
-<td style="width: 149px;"><strong>Main repository</strong></td>
+<td style="width: 109.417px;"><strong>Main repository</strong></td>
+<td style="width: 202.583px;"><strong>Amount of broken packages<br /></strong></td>
 </tr>
 <tr>
-<td style="width: 149px;"><a href="http://repoclosure.openmandriva.org/repoclosure-i686.html">Cooker i686</a></td>
+<td style="width: 109.417px;"><a href="http://repoclosure.openmandriva.org/repoclosure-i686.html">Cooker i686</a></td>
+<td style="width: 202.583px;">broken-i686</td>
 </tr>
 <tr>
-<td style="width: 149px;"><a href="http://repoclosure.openmandriva.org/repoclosure-x86_64.html">Cooker x86_64</a></td>
+<td style="width: 109.417px;"><a href="http://repoclosure.openmandriva.org/repoclosure-x86_64.html">Cooker x86_64</a></td>
+<td style="width: 202.583px;">broken-x86_64</td>
 </tr>
 <tr>
-<td style="width: 149px;"><a href="http://repoclosure.openmandriva.org/repoclosure-armv7hnl.html">Cooker armv7hnl</a></td>
+<td style="width: 109.417px;"><a href="http://repoclosure.openmandriva.org/repoclosure-armv7hnl.html">Cooker armv7hnl</a></td>
+<td style="width: 202.583px;">broken-armv7hl</td>
 </tr>
 <tr>
-<td style="width: 149px;"><a href="http://repoclosure.openmandriva.org/repoclosure-aarch64.html">Cooker aarch64</a></td>
+<td style="width: 109.417px;"><a href="http://repoclosure.openmandriva.org/repoclosure-aarch64.html">Cooker aarch64</a></td>
+<td style="width: 202.583px;">broken-aarch64</td>
 </tr>
 <tr>
-<td style="width: 149px;"><a href="http://repoclosure.openmandriva.org/repoclosure-znver1.html">Cooker znver1</a></td>
+<td style="width: 109.417px;"><a href="http://repoclosure.openmandriva.org/repoclosure-znver1.html">Cooker znver1</a></td>
+<td style="width: 202.583px;">broken-znver1</td>
 </tr>
 </tbody>
 </table>
@@ -67,5 +73,7 @@ EOF
 # (tpg) HTMLify it
     sed -i -e 's#^package: #<hr /><strong>package: </strong> #g' -e 's,unresolved deps:,<span style="color: #ff0000;"><strong>unresolved deps:</strong></span> ,g' /repoclosure-report/repoclosure-"$i".html
     printf '%s\n' '</body></html>' >> /repoclosure-report/repoclosure-"$i".html
+    amount=$(grep -r "package:" /repoclosure-report/repoclosure-"$i".html | wc -l)
+    sed -i -e "s/$amount/broken-$i/g" /repoclosure-report/repoclosure-"$i".html
 done
 
