0717658dd1ac6a4586804819b72cedb3b78aa557
Bernhard Rosenkränzer <bero@lindev.ch>
2021-02-28 01:56:14 +0100

    Make everything build with current cooker; push arch specific tags

--- make_containers.sh
+++ make_containers.sh
@@ -1,47 +1,52 @@
+ARCH=$(uname -m)
 
 pushd abf-createrepo/
-	docker build --tag=openmandriva/createrepo --file Dockerfile.createrepo .
+	docker build --tag=openmandriva/createrepo:${ARCH} --file Dockerfile.createrepo .
 popd
 
 pushd publisher
-	docker build --tag=openmandriva/publisher --file Dockerfile.publisher .
+	docker build --tag=openmandriva/publisher:${ARCH} --file Dockerfile.publisher .
 popd
 
-pushd abf-genhdlists
-	docker build --tag=openmandriva/genhdlists2 --file Dockerfile.genhdlists2 .
-popd
+#pushd abf-genhdlists
+#	docker build --tag=openmandriva/genhdlists2:${ARCH} --file Dockerfile.genhdlists2 .
+#popd
 
 pushd abf-nginx
-	docker build --tag=openmandriva/nginx --file Dockerfile .
+	docker build --tag=openmandriva/nginx:${ARCH} --file Dockerfile .
 popd
 
 pushd abf-repoclosure
-	docker build --tag=openmandriva/repoclosure --file Dockerfile.repoclosure .
+	docker build --tag=openmandriva/repoclosure:${ARCH} --file Dockerfile.repoclosure .
 popd
 
 pushd abf-service
-	docker build --tag=openmandriva/abf --file Dockerfile .
+	docker build --tag=openmandriva/abf:${ARCH} --file Dockerfile .
 popd
 
 pushd abf-service-sidekiq
-	docker build --tag=openmandriva/abf-service-sidekiq-worker:latest --file Dockerfile .
+	docker build --tag=openmandriva/abf-service-sidekiq-worker:${ARCH} --file Dockerfile .
 popd
 
 pushd iso-builder
-	docker build --tag=openmandriva/isobuilder --file Dockerfile.isobuilder .
+	docker build --tag=openmandriva/isobuilder:${ARCH} --file Dockerfile.isobuilder .
 popd
 
 pushd oma-file-store
-	docker build --tag=openmandriva/file-store --file Dockerfile .
+	docker build --tag=openmandriva/file-store:${ARCH} --file Dockerfile .
 popd
 
-docker push openmandriva/createrepo
-docker push openmandriva/publisher
-docker push openmandriva/genhdlists2
-docker push openmandriva/nginx
-docker push openmandriva/repoclosure
-docker push openmandriva/abf
-docker push openmandriva/abf-service-sidekiq-worker
-docker push openmandriva/isobuilder
-docker push openmandriva/file-store
+pushd abf-redis
+	docker build --tag=openmandriva/redis:${ARCH} --file Dockerfile .
+popd
 
+docker push openmandriva/createrepo:${ARCH}
+docker push openmandriva/publisher:${ARCH}
+docker push openmandriva/genhdlists2:${ARCH}
+docker push openmandriva/nginx:${ARCH}
+docker push openmandriva/repoclosure:${ARCH}
+docker push openmandriva/abf:${ARCH}
+docker push openmandriva/abf-service-sidekiq-worker:${ARCH}
+docker push openmandriva/isobuilder:${ARCH}
+docker push openmandriva/file-store:${ARCH}
+docker push openmandriva/redis:${ARCH}
